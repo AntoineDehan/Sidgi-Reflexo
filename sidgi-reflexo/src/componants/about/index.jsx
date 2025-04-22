a8878f598213b52ce5bb9f97c35601b76ef5bd32
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-04-22 14:32:04 +0200

    Landing page amélioré

--- sidgi-reflexo/src/componants/about/index.jsx
+++ sidgi-reflexo/src/componants/about/index.jsx
@@ -3,19 +3,17 @@ import "../../styles/css/about/style.css";
 function About({ data }) {
   return (
     <div className="about-conteneur">
-      <div className="test">
-        <img src={data.image} alt={data.titre} className="about-image" />
-        <div className="about-content">
-          <h2>{data.titre}</h2>
-          <p>
-            {data.content.split("\n").map((line, index) => (
-              <span key={index}>
-                {line}
-                <br />
-              </span>
-            ))}
-          </p>
-        </div>
+      <img src={data.image} alt={data.titre} className="about-image" />
+      <div className="about-content">
+        <h2>{data.titre}</h2>
+        <p>
+          {data.content.split("\n").map((line, index) => (
+            <span key={index}>
+              {line}
+              <br />
+            </span>
+          ))}
+        </p>
       </div>
     </div>
   );
--- sidgi-reflexo/src/componants/seocontent/index.jsx
+++ sidgi-reflexo/src/componants/seocontent/index.jsx
@@ -1,8 +1,17 @@
+import "../../styles/css/content/style.css";
+
 function Seocontent({ data }) {
   return (
     <div className="seo-conteneur">
       <h2>{data.titre}</h2>
-      <p>{data.content}</p>
+      <p>
+        {data.content.split("\n").map((line, index) => (
+          <span key={index}>
+            {line}
+            <br />
+          </span>
+        ))}
+      </p>
     </div>
   );
 }
