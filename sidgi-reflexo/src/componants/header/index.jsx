a2304d19824583b3839b4b039359addd655d2aa2
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-07-02 13:40:46 +0200

    Ajout Font Awesome Début menu burger pour mobile Ajout gitignore

--- sidgi-reflexo/src/componants/header/index.jsx
+++ sidgi-reflexo/src/componants/header/index.jsx
@@ -6,15 +6,22 @@ import LogoImg from "../../assets/logo_header.webp";
 function Header() {
   return (
     <header>
-      <img
-        className="logo"
-        src={LogoImg}
-        alt="Logo sobre de l'entreprise Digi-Réflexo"
-      />
-      <nav>
-        <a href="#accueil">Accueil</a>
-        <a href="#offres">Offres</a>
-        <a href="#reflexologie">La Rélexologie</a>
+      <nav className="navbar">
+        <div className="navbar__logo">
+          <img
+            className="logo"
+            src={LogoImg}
+            alt="Logo sobre de l'entreprise Sidgi-Réflexo"
+          />
+        </div>
+        <div className="navbar__nav">
+          <button className="burger">XX</button>
+          <div className="desktop">
+            <a href="#accueil">Accueil</a>
+            <a href="#offres">Offres</a>
+            <a href="#reflexologie">La Rélexologie</a>
+          </div>
+        </div>
       </nav>
     </header>
   );
