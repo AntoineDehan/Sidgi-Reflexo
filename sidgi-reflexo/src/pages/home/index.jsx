d71dbc135b81f44a108639858275fc8ec2706e3b
AntoineDehan <antoine.dehandartial@gmail.com>
2025-10-06 14:16:16 +0200

    fix(offre): popup supprimé et changements données

--- sidgi-reflexo/src/pages/home/index.jsx
+++ sidgi-reflexo/src/pages/home/index.jsx
@@ -15,17 +15,18 @@ import Localisation from "../../componants/localisation";
 import "../../styles/scss/home/style.scss";
 
 function Home({ isMenuOpen, setIsMenuOpen }) {
-  const [isPopupOpen, setPopupOpen] = useState(false);
+  ///Gestion de popup - si desactivé = pas d'offre
+  // const [isPopupOpen, setPopupOpen] = useState(false);
 
-  useEffect(() => {
-    if (!sessionStorage.getItem("offer-popup-shown")) {
-      const timer = setTimeout(() => {
-        setPopupOpen(true);
-        sessionStorage.setItem("offer-popup-shown", "true");
-      }, 3000);
-      return () => clearTimeout(timer);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (!sessionStorage.getItem("offer-popup-shown")) {
+  //     const timer = setTimeout(() => {
+  //       setPopupOpen(true);
+  //       sessionStorage.setItem("offer-popup-shown", "true");
+  //     }, 3000);
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, []);
   return (
     <div className="main-conteneur">
       {isMenuOpen && (
@@ -51,13 +52,6 @@ function Home({ isMenuOpen, setIsMenuOpen }) {
           <Localisation />
         </section>
       </div>
-
-      <Modal isOpen={isPopupOpen} onClose={() => setPopupOpen(false)}>
-        <p className="modal-text">
-          Je propose une offre de lancement pour les débuts de Sidgi Réflexo!
-        </p>
-        <Prestation data={PopupData}></Prestation>
-      </Modal>
     </div>
   );
 }
